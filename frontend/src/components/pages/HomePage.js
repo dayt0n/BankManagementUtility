cc181ab88424e416f51e5cf2955b907e0dbfba37
daltonjohnson7 <60797380+daltonjohnson7@users.noreply.github.com>
2022-03-20 00:06:21 -0500

    heres the home page with login, no actual login/forgot password functionality yet

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,15 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import logo from '../logo.png';
+import './HomePage.css';
 
-function HomePage() {
-    return (
-        <div className="Home">
-            <div>
-                <p>Home Page</p>
-                <Link to="/test">Click me!</Link>
-            </div>
+function StaticText() {
+  return (
+    <div className="App">
+      <header className="AppHeader">
+        <img src={logo} className="BankLogo" />
+        <p>
+          Bank
+        </p>
+        <div className="LoginText">
+          <div className="NewUser">
+            <li>
+              New
+            </li>
+            <li>
+              Customer
+            </li>
+            <br></br>
+            <button className="Button">Create Account</button>
+          </div>
+          <div className="ExistingUser">
+            <li>
+              Existing
+            </li>
+            <li>
+              Customer
+            </li>
+            <br></br>
+            <label for="Uname">Username:</label> 
+            <br></br>
+            <input type="text" id="Uname" name="Uname"></input> 
+            <br></br>
+            <label for="Pword">Password:</label>
+            <br></br>
+            <input type="password" id="Pword" name="Pword"></input>
+            <br></br>
+            <br></br>
+            <button className="Button">Log In</button>
+            <p>
+              Forgot Password?
+            </p>
+          </div>
         </div>
-    );
+      </header>
+    </div>
+  );
 }
 
 export default HomePage;
