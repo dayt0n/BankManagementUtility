c04f2fa23dc59b6d7dbbf6978dd74023a1b3794d
daltonjohnson7 <60797380+daltonjohnson7@users.noreply.github.com>
2022-03-30 15:20:51 -0500

    login page uses forms now and actually talks to api wow

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,7 +1,16 @@
 import logo from '../logo.png';
 import './HomePage.css';
+import { Form, Message } from "semantic-ui-react";
+import React, { useState } from "react";
 
 function HomePage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(Boolean);
+  const [error, setError] = useState(Boolean);
+  const [requestLoading, setRequestLoading] = useState(Boolean);
+  var errorMsg = 'Placeholder Error Message';
+
   return (
     <div className="HomePage">
       <header className="AppHeader">
@@ -28,16 +37,94 @@ function HomePage() {
               Customer
             </li>
             <br></br>
-            <label for="Uname">Username:</label> 
-            <br></br>
-            <input type="text" id="Uname" name="Uname"></input> 
-            <br></br>
-            <label for="Pword">Password:</label>
-            <br></br>
-            <input type="password" id="Pword" name="Pword"></input>
-            <br></br>
-            <br></br>
-            <button className="Button">Log In</button>
+            <Form inverted className="UserLoginForm" success={success} error={error} >
+                <Form.Group widths='equal'>
+                    <Form.Input
+                        required
+                        fluid
+                        label='Username'
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group widths='equal'>
+                    <Form.Input
+                        required
+                        fluid
+                        type='password'
+                        label='Password'
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Button
+                    fluid
+                    loading={requestLoading}
+                    type='submit'
+                    onClick={async () => {
+                        const createRequest = { username, password };
+                        var quit = false;
+                        for (var field in createRequest) {
+                            if (createRequest[field] === "") {
+                                errorMsg = document.getElementById('Error Message').getElementsByTagName('p')
+                                errorMsg[0].textContent = "All fields must be filled."
+                                setError(true);
+                                setSuccess(false);
+                                quit = true;
+                            }
+                        }
+
+                        if (quit) { return; }
+
+
+                        const response = await fetch("/api/auth/login", {
+                            method: "POST",
+                            headers: {
+                                "Accept": "application/json",
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(createRequest),
+                        });
+
+                        if (!response.ok) {
+                            console.log("response failed!");
+                            errorMsg = document.getElementById('Error Message').getElementsByTagName('p')
+                            errorMsg[0].textContent = "No response from server. Check your internet connection and try again."
+                            setError(true);
+                            setSuccess(false);
+                            return;
+                        }
+                        setRequestLoading(true);
+                        let content = await response.json();
+                        setRequestLoading(false);
+                        
+                        if (content.status === 'error') {
+                            errorMsg = document.getElementById('Error Message').getElementsByTagName('p')
+                            errorMsg[0].textContent = content.msg
+                            setError(true);
+                            setSuccess(false);
+                        }
+                        else if (content.status === 'success') {
+                            setError(false);
+                            setSuccess(true);
+                        }
+                    }}
+                >
+                    Log In
+                </Form.Button>
+                <Message
+                    success
+                    header='Form Completed'
+                    content='Successfully logged in.'
+                />
+                <Message id='Error Message'
+                    error
+                    header='Error!'
+                    content='Placeholder Error Message.'
+                />
+            </Form>
             <p>
               Forgot Password?
             </p>
