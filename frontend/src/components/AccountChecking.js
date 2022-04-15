5d1b97e82989b2cf7b4ebe25d5f257d917c483aa
daltonjohnson7 <60797380+daltonjohnson7@users.noreply.github.com>
2022-04-15 05:56:12 -0500

    probably working but ugly checking account summary

--- frontend/src/components/AccountChecking.js
+++ frontend/src/components/AccountChecking.js
@@ -3,9 +3,29 @@ import "./Account.css"
 
 function AccountChecking(account) {
 
+    const history = fetch("/api/money/account/history/" + account["accountNum"] + "/5");
+
     return (
         <div>
-
+            <header>
+                {account["accountName"]}
+                Routing: {account["routingNumber"]}
+                Account: {account["accountNum"]}
+            </header>
+            <ul>
+                Balance: {account["balance"]}
+            </ul>
+            <ul>
+                Dividend: {account["dividendRate"]}
+            </ul>
+            <ul>
+                Recent Transactions
+                {history[0]}
+                {history[1]}
+                {history[2]}
+                {history[3]}
+                {history[4]}
+            </ul>
         </div>
     )
 }
