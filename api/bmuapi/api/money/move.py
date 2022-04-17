5af642dfc4854294d6456f75e5e2e5b86d737199
dayt0n <dayt0n@dayt0n.com>
2022-04-16 23:27:53 -0500

    more WIP transfer stuff

--- api/bmuapi/api/money/move.py
+++ api/bmuapi/api/money/move.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, abort, request
 
 from bmuapi.utils import requires_auth
+from bmuapi.database.ops import transfer_op
+from bmuapi.api.api_utils import success, error
 
 move = Blueprint('move', __name__, url_prefix='/move')
 
@@ -14,7 +16,18 @@ def move_home():
 @requires_auth
 def transfer(token):
     data = dict(request.get_json())
-    if not all(k in data for k in ('from', 'to', 'amount')):
+    if not all(k in data for k in ('from', 'amount')):
         abort(500)
+    fromID = int(data['from'])
+    amount = int(data['amount'])
+    if amount <= 0: # hashtag security
+        return error(f"Invalid transfer amount: {amount}")
+    if 'to' in data:
+        toID = int(data['to'])
+    if 'comment' in data:
+        comment = data['comment']
+    
+    # TODO: CHECK HERE IF USER OWNS FROM ACCOUNT
+    transfer_op(amount,)
     # do user checks here
     # then call transfer_op
--- api/bmuapi/database/ops.py
+++ api/bmuapi/database/ops.py
@@ -3,10 +3,13 @@ from bmuapi.database.database import SessionManager, get_money_account
 from bmuapi.database.tables import UserAccount, Mortgage, MoneyMarket, TransactionHistory
 from sqlalchemy import and_
 
+from api.bmuapi.database.tables import CreditCard
 
-def transfer_op(fromAccount, toAccount, amount, session=None):
+
+def transfer_op(amount, fromAccount, toAccount=None, session=None,comment="withdraw"):
     frm: UserAccount = fromAccount
-    to: UserAccount = toAccount
+    if toAccount:
+        to: UserAccount = toAccount
     if isinstance(fromAccount, int):
         with SessionManager(commit=False) as sess:
             if not session:
@@ -15,17 +18,19 @@ def transfer_op(fromAccount, toAccount, amount, session=None):
                 UserAccount.accountNum == fromAccount).first()
         if not frm:
             return f"Account {fromAccount} not found."
-    if isinstance(toAccount, int):
-        with SessionManager(commit=False) as sess:
-            if not session:
-                session = sess
-            to = session.query(UserAccount).filter(
-                UserAccount.accountNum == toAccount).first()
-            if not to:
-                return f"Account {toAccount} not found."
+    if toAccount:
+        if isinstance(toAccount, int):
+            with SessionManager(commit=False) as sess:
+                if not session:
+                    session = sess
+                to = session.query(UserAccount).filter(
+                    UserAccount.accountNum == toAccount).first()
+                if not to:
+                    return f"Account {toAccount} not found."
     fromMoney = get_money_account(frm, session=session)
-    toMoney = get_money_account(to, session=session)
-    if not toMoney or not fromMoney:
+    if toAccount:
+        toMoney = get_money_account(to, session=session)
+    if (toAccount and not toMoney) or not fromMoney:
         return "Could not locate money accounts to complete transfer."
     if isinstance(fromMoney, Mortgage):  # cannot transfer money FROM a mortgage account
         return "Cannot transfer money from a mortgage account."
@@ -38,16 +43,25 @@ def transfer_op(fromAccount, toAccount, amount, session=None):
                                                                     TransactionHistory.transactionDate > oneMonthAgo)).count()
             if history >= 6:
                 return f"Transfer limit reached on Money Market account {frm.accountNum}"
-        if isinstance(toMoney, MoneyMarket):
-            history = session.query(TransactionHistory).filter(and_(TransactionHistory.accountID == frm.id,
-                                                                    TransactionHistory.transactionDate > oneMonthAgo)).count()
-            if history >= 6:
-                return f"Transfer limit reached on Money Market account {to.accountNum}."
+        if toAccount:
+            if isinstance(toMoney, MoneyMarket):
+                history = session.query(TransactionHistory).filter(and_(TransactionHistory.accountID == frm.id,
+                                                                        TransactionHistory.transactionDate > oneMonthAgo)).count()
+                if history >= 6:
+                    return f"Transfer limit reached on Money Market account {to.accountNum}."
         if fromMoney.balance < amount:
             return f"Insufficient funds in {frm.accountNum}."
         fromMoney.balance -= amount
-        if isinstance(toMoney, Mortgage):
-            toMoney.currentAmountOwed -= amount
-        else:
-            toMoney.balance += amount
+        fromRecipient = toMoney.accountName + f"({to.accountNum})" if toAccount else comment
+        fromIsInternal = True if toAccount else False
+        tdate = arrow.utcnow().datetime
+        fromTH = TransactionHistory(accountID=frm.id, recipient=fromRecipient, transactionDate=tdate, amount=amount, internal=fromIsInternal)
+        if toAccount:
+            if isinstance(toMoney, Mortgage):
+                toMoney.currentAmountOwed -= amount
+            else:
+                toMoney.balance += amount
+            toTH = TransactionHistory(accountID=to.id, recipient=fromRecipient,transactionDate=tdate, amount=amount, internal=fromIsInternal)
+            session.add(toTH)
+        session.add(fromTH)
     return amount
